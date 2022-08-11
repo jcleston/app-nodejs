48204877acda307a31bdd30b7eed9d149c956912
jcleston <janes.cleston.silva@gmail.com>
2022-08-11 11:15:03 -0300

    Criando a classe App, o construtor e adicionando o server

--- src/app.js
+++ src/app.js
@@ -1,2 +1,15 @@
+// Arquivo de inicio da aplicação
 
+// Importando o express
+const express = require("express");
+
+// Criando a class App
+class App{
+
+    //Criando o construtor
+    constructor(){
+        // Adicionando a propriedade server
+        this.server = express();
+    }
+}
 
