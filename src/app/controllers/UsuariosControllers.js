a32159da6b6104361736a44e3b12462da5b105dc
jcleston <janes.cleston.silva@gmail.com>
2022-08-11 13:09:49 -0300

    Adicionando o controller do usuario

---
+++ src/app/controllers/UsuariosControllers.js
@@ -0,0 +1,29 @@
+// Criando a classe usuarios
+class UsuariosController {
+
+    // Método para listar todos os usuarios
+    index(req, res) {
+
+    }
+
+    // Método para recuperar um usuario
+    show(req, res) {
+
+    }
+
+    // Método para criar um usuario
+    create(req, res) {
+
+    }
+
+    // Método para atualizar um usuario
+    update(req, res) {
+
+    }
+
+    // Método para excluir um usuario
+    delete(req, res) {
+
+    }
+
+}
