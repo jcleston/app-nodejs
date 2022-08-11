ff844b7bc67ce0dad4a92ef22d4b3d7f28bc6e11
jcleston <janes.cleston.silva@gmail.com>
2022-08-11 11:51:44 -0300

    Criando a primeira rota /

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,10 @@ const { Router } = require("express");
 // Criando/Instanciando a rota
 const routes = new Router();
 
-
+// Criando a rota hello
+routes.get("/", (req, res) => {
+    return res.json({ message: "Hello" });
+});
 
 // Exportando as rotas
 module.exports = routes;
