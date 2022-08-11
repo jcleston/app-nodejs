05cc85ff72d6ca57ea92c55df8c627d5857f5b63
jcleston <janes.cleston.silva@gmail.com>
2022-08-11 13:23:21 -0300

    Criando as outras rotas restantes para usuario

--- src/routes.js
+++ src/routes.js
@@ -7,8 +7,21 @@ const routes = new Router();
 // Importando a classe UsuariosController
 const usuarios = require("./app/controllers/UsuariosControllers");
 
-// Criando a rota index para usuarios
+// Criando a rota index para retornar todos os usuarios
 routes.get("/usuarios", usuarios.index);
+
+// Criando a rota show para retornar um usuario
+routes.get("/usuarios/:id", usuarios.show);
+
+// Criando a rota create para criar um usuario
+routes.post("/usuarios", usuarios.create);
+
+// Criando a rota update para atualizar um usuario
+routes.put("/usuarios/:id", usuarios.update);
+
+// Criando a rota delete para excluir um usuario
+routes.delete("/usuarios/:id", usuarios.delete);
+
 
 // Exportando as rotas
 module.exports = routes;
