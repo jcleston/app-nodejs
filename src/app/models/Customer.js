0261c45af94e55d3023dcc628733423fe88d99b6
jcleston <janes.cleston.silva@gmail.com>
2022-08-19 15:05:43 -0300

    Configuração da classe Customer

---
+++ src/app/models/Customer.js
@@ -0,0 +1,22 @@
+// Importando o Model
+import Sequelize, { Model } from "sequelize";
+
+// Criando a classe Customer
+class Customer extends Model {
+  // Criando o método de inicialização
+  static init(sequelize) {
+    super.init(
+      {
+        // Configurando
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        status: Sequelize.ENUM("ACTIVE", "ARCHIVED"),
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+}
+
+export default Customer;
